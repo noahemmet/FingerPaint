36f998220d0217011d6084bde60eda3b1f9c12c8
Noah Emmet <noah@noahemmet.com>
2016-04-20 18:57:00 -0700

    Initial Commit

---
+++ FingerPaint/FingerPaint.h
@@ -0,0 +1,19 @@
+//
+//  FingerPaint.h
+//  FingerPaint
+//
+//  Created by Noah Emmet on 4/20/16.
+//  Copyright © 2016 Sticks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FingerPaint.
+FOUNDATION_EXPORT double FingerPaintVersionNumber;
+
+//! Project version string for FingerPaint.
+FOUNDATION_EXPORT const unsigned char FingerPaintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FingerPaint/PublicHeader.h>
+
+
