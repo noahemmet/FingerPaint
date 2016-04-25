12450a4f7f9739df5ff122cc32500d083324ebaa
Noah Emmet <noah@noahemmet.com>
2016-04-24 17:00:21 -0700

    drawing

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -12,29 +12,73 @@ import FingerPaint
 class ViewController: UIViewController {
 	
 	@IBOutlet var canvasView: CanvasView!
+	var path: UIBezierPath?
+	var shapeLayer: CAShapeLayer?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		canvasView.canvasDelegate = self
+//		canvasView.canvasDelegate = self
+		let paintGestureRecognizer = PaintGestureRecognizer(target: self, action: #selector(ViewController.touched(_:)))
+		view.addGestureRecognizer(paintGestureRecognizer)
+		
+		
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	func touched(paintGestureRecognizer: PaintGestureRecognizer) {
+		switch paintGestureRecognizer.state {
+		case .Began:
+			print("vc touch began")
+			let path = UIBezierPath()
+			switch paintGestureRecognizer.anchorState {
+			case .None:
+				print("unreachable")
+			case .Single(let touch):
+				path.moveToPoint(touch.locationInView(view))
+			case .Double(let first, let second):
+				path.moveToPoint(first.locationInView(view))
+				path.moveToPoint(second.locationInView(view))
+			}
+			self.path = path
+			let shapeLayer = CAShapeLayer()
+			shapeLayer.path = path.CGPath
+			shapeLayer.strokeColor = UIColor.purpleColor().CGColor
+			shapeLayer.lineWidth = 3
+			self.shapeLayer = shapeLayer
+			view.layer.addSublayer(shapeLayer)
+		case .Changed:
+			print("vc touch changed")
+			switch paintGestureRecognizer.anchorState {
+			case .None:
+				print("unreachable")
+			case .Single(let touch):
+				path?.addLineToPoint(touch.locationInView(view))
+				shapeLayer?.path = path?.CGPath
+			case .Double(let first, let second):
+				path?.addLineToPoint(first.locationInView(view))
+				path?.addLineToPoint(second.locationInView(view))
+			}
+		default:
+			break
+		}
+	}
 }
 
 
-extension ViewController: CanvasDelegate {
-	func touchPathCreated(touchPath: TouchPath) {
-		print("created", touchPath)
-	}
-	
-	func touchPathUpdated(touchPath: TouchPath) {
-		print("updated", touchPath)
-	}
-	
-	func touchPathEnded(touchPath: TouchPath) {
-		print("ended", touchPath)
-	}
+extension ViewController {
+//	func touchPathCreated(touchPath: TouchPath) {
+//		print("created", touchPath)
+//	}
+//	
+//	func touchPathUpdated(touchPath: TouchPath) {
+//		print("updated", touchPath)
+//	}
+//	
+//	func touchPathEnded(touchPath: TouchPath) {
+//		print("ended", touchPath)
+//	}
 }
--- FingerPaint/PaintGestureRecognizer.swift
+++ FingerPaint/PaintGestureRecognizer.swift
@@ -6,8 +6,101 @@
 //  Copyright © 2016 Sticks. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import UIKit.UIGestureRecognizerSubclass
 
-class PaintGestureRecognizer: UIGestureRecognizer {
+public class PaintGestureRecognizer: UIGestureRecognizer {
 	
+	public enum AnchorState {
+		case None
+		case Single(touch: UITouch)
+		case Double(first: UITouch, second: UITouch)
+	}
+	
+	public enum PaintPhase {
+		case AnchoredLine
+		case UnanchoredLine
+		case Draw
+	}
+	
+	public var anchorState: AnchorState = .None
+	var paintPhase: PaintPhase?
+	var firstAnchor: UITouch?
+	var secondAnchor: UITouch?
+	
+	public override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent) {
+		super.touchesBegan(touches, withEvent: event)
+		let allTouches = Array(touches)
+		switch allTouches.count {
+		case 1:
+			switch anchorState {
+			case .None:
+				print("1 began from .None to .Single")
+				anchorState = .Single(touch: allTouches[0])
+				state = .Began
+			case .Single(let touch):
+				print("1 began from .Single to .Double")
+				anchorState = .Double(first: touch, second: allTouches[0])
+			case .Double(_, _):
+				print("1 began from .Double to .None (failed)")
+				anchorState = .None
+				state = .Failed
+			}
+		case 2:
+			switch anchorState {
+			case .None:
+				print("2 began from .None to .Double")
+				anchorState = .Double(first: allTouches[0], second: allTouches[1])
+				state = .Began
+			case .Single(_), .Double(_, _):
+				print("2 began from .Single/Double to .None (failed)")
+				anchorState = .None
+				state = .Failed
+			}
+		default:
+			print("default began (failed)")
+			state = .Failed
+		}
+	}
+	
+	override public func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent) {
+		super.touchesMoved(touches, withEvent: event)
+		state = .Changed
+	}
+	
+	override public func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent) {
+		super.touchesEnded(touches, withEvent: event)
+		switch touches.count {
+		case 1:
+			switch anchorState {
+			case .None:
+				print("1 ended from .None (failed)")
+				state = .Failed
+			case .Single(_):
+				print("1 ended from .Single to .None")
+				anchorState = .None
+				state = .Ended
+			case .Double(_, let secondTouch):
+				print("1 ended from .Double to .Single")
+				anchorState = .Single(touch: secondTouch)
+			}
+		case 2:
+			switch anchorState {
+			case .None:
+				print("2 ended from .None (failed)")
+				state = .Failed
+			case .Single(_):
+				print("2 ended .Single (failed)")
+				anchorState = .None
+				state = .Failed
+			case .Double(_, let secondTouch):
+				print("2 ended from .Double to .Single")
+				anchorState = .Single(touch: secondTouch)
+			}
+		default:
+			print("default ended (failed)")
+			break
+		}
+		
+	}
 }
